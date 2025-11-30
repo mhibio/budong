8a02e311a4c2a2d823127ac4364ec2193b72ce75
ThuMyatHtoo29 <153301599+ThuMyatHtoo29@users.noreply.github.com>
2025-12-01 00:27:02 +0900

    fixed

--- BUDONG/api/routers/v1/user/saved_buildings.py
+++ BUDONG/api/routers/v1/user/saved_buildings.py
@@ -1,17 +1,20 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session
 from BUDONG.api.core.database import get_db
-from BUDONG.api.models import TUserSavedBuilding
+from BUDONG.api.models import TUser, TUserSavedBuilding
+from BUDONG.api.core.auth import get_current_active_user
 from BUDONG.api.schemas.schema_saved_buildings import SavedBuildingsResponse
 
 router = APIRouter()
 
 @router.get("/saved-buildings", response_model=SavedBuildingsResponse)
-def get_saved_buildings(user_id: int, db: Session = Depends(get_db)):
+def get_saved_buildings(
+    db: Session = Depends(get_db),
+    current_user: TUser = Depends(get_current_active_user)  # 🔥 Token-based user
+):
     saved_buildings = (
         db.query(TUserSavedBuilding)
-        .options(joinedload(TUserSavedBuilding.building))  # 🔥 JOIN building table
-        .filter(TUserSavedBuilding.user_id == user_id)
+        .filter(TUserSavedBuilding.user_id == current_user.user_id)  # 💯 real user
         .all()
     )
 
